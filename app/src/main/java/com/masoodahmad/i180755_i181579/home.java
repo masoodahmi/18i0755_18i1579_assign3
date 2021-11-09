dfbaa4b658719e9285d8fb462a59f5dc75b6f0b1
haseebahmad3399 <i181579@nu.edu.pk>
2021-11-09 18:53:27 +0500

    logoutimplement

--- app/src/main/java/com/masoodahmad/i180755_i181579/home.java
+++ app/src/main/java/com/masoodahmad/i180755_i181579/home.java
@@ -2,6 +2,7 @@ package com.masoodahmad.i180755_i181579;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -14,6 +15,9 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -22,21 +26,32 @@ import java.util.List;
 
 public class home extends AppCompatActivity {
     RecyclerView rv;
+    ImageView logg;
     List<homee> ls;
-
+    TextView logout;
+    DrawerLayout drwr;
+    SManager sManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-
-
-
+        drwr = findViewById(R.id.drwr);
+        sManager = new SManager(getApplicationContext());
         BottomNavigationView bottomnav=findViewById(R.id.botmnav);
         bottomnav.setOnNavigationItemSelectedListener(navListener);
 
         getSupportFragmentManager().beginTransaction().replace(R.id.fragmntcont,new homefragment()).commit();
-
+        logout=findViewById(R.id.logout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sManager.setLogin(false);
+                sManager.setUsername("");
+                startActivity(new Intent(home.this,login.class));
+                finish();
+            }
+        });
     }
     private BottomNavigationView.OnNavigationItemSelectedListener navListener=
             new BottomNavigationView.OnNavigationItemSelectedListener() {
